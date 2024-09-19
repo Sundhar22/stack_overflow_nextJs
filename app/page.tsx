b085fc6bd1e8a13c81441e90666143e2f92af42e
Sundhar22 <mohanasundharamofficial@gmail.com>
2024-09-19 11:57:40 +0530

    included auth

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ export default function Home() {
   return (
     <>
       <div className="h1-bold dark-gradient">we are coming </div>
-      <div className="h2-bold dark-gradient">we are coming </div>
-      <div className="h3-bold dark-gradient">we are coming </div>
     </>
   );
 }
